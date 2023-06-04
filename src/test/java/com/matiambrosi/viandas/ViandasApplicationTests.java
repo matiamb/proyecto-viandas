2d20dd78b56586aadc8ff7062c54f5b54dbd8318
Matias Ambrosi <matiambrosi@gmail.com>
2023-06-04 02:40:01 -0300

    Primer commit, con clase cliente y paquetes creados

---
+++ src/test/java/com/matiambrosi/viandas/ViandasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matiambrosi.viandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViandasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
