8514f46e2cc2b50186bca6515dc4a1cf16d0c6f6
matiambrosi <matiambrosi@pop-os.localdomain>
2023-06-04 05:39:40 -0300

    Faltaba la annotation

--- proyecto-viandas/src/main/java/com/matiambrosi/viandas/repository/IComidaRepository.java
+++ proyecto-viandas/src/main/java/com/matiambrosi/viandas/repository/IComidaRepository.java
@@ -3,7 +3,9 @@ package com.matiambrosi.viandas.repository;
 
 import com.matiambrosi.viandas.model.Comida;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IComidaRepository extends JpaRepository <Comida, Long>{
     
 }
