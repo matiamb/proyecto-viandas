17f2a24fdec4f35705ef6b6d3199a2ff7141cd59
Matias Ambrosi <matiambrosi@gmail.com>
2023-06-04 21:59:40 -0300

    Relacion con comida creada y agregado su constructor

--- proyecto-viandas/src/main/java/com/matiambrosi/viandas/model/Pedido.java
+++ proyecto-viandas/src/main/java/com/matiambrosi/viandas/model/Pedido.java
@@ -5,7 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,10 +20,16 @@ public class Pedido {
     private Long id;
     @OneToMany
     public Cliente unCliente;
-    public Comida unaComida;
+    @ManyToMany
+    public List<Comida> comidas;
 
     public Pedido() {
     }
-    
+
+    public Pedido(Long id, Cliente unCliente, List<Comida> comidas) {
+        this.id = id;
+        this.unCliente = unCliente;
+        this.comidas = comidas;
+    }
     
 }
